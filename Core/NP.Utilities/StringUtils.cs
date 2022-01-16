23f086230fad21ac47e9e99598d4a97441ad0979
npolyak <nick.polyak@verizon.net>
2022-01-16 01:35:53 -0500

    removed commented out method

--- Core/NP.Utilities/StringUtils.cs
+++ Core/NP.Utilities/StringUtils.cs
@@ -40,30 +40,5 @@
 
             return (startStrPart, endStrPart);
         }
-
-        //public static (string startStrPart, string endStrPart) BreakStringIntoTwoParts(this string str, string separator)
-        //{
-        //    int separatorIdx = str.Length;
-        //    int endPartBeginIdx = str.Length;
-
-        //    if (separator != null)
-        //    {
-        //        int realSeparatorIdx = str.IndexOf(separator);
-
-        //        if (realSeparatorIdx != -1)
-        //        {
-        //            separatorIdx = realSeparatorIdx;
-
-        //            endPartBeginIdx = separatorIdx + separator.Length;
-        //        }
-        //    }
-
-        //    string startStrPart = str.Substring(0, separatorIdx);
-
-        //    string endStrPart = str.Substring(endPartBeginIdx);
-
-        //    return (startStrPart, endStrPart);
-        //}
-
     }
 }
